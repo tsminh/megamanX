f3b7223fa5fcd0aabd6fc31196d25598016c9b75
Tô Hiếu Minh <minh.to@eyeq.tech>
2019-10-27 22:38:41 +0700

    update week 3

---
+++ Assets/cameraControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControler : MonoBehaviour
+{
+    Rigidbody2D _rigid;
+    public GameObject character;
+    float left = -1;
+    Vector3 right = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(character.transform.position.x, character.transform.position.y, -1);
+        
+        RaycastHit2D hitInfo1 = Physics2D.Raycast(transform.position, Vector2.left, 1.25f, 1<<9);
+        Debug.DrawRay(transform.position, Vector2.left * 1.25f, Color.red);
+        if (hitInfo1.collider != null) {
+            if (left == -1) {
+                left = transform.position.x;
+            } else {
+                transform.position = new Vector3(left, transform.position.y, transform.position.z);
+            } 
+            // Debug.Log("Hit: " + hitInfo1.collider.name);
+        } else {
+            if (character.transform.position.x >= left) {
+                transform.position = new Vector3(character.transform.position.x, character.transform.position.y, -1);
+            }
+        }
+        // _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+    }
+}
--- Assets/mario_script.cs
+++ Assets/mario_script.cs
@@ -5,36 +5,65 @@ using UnityEngine;
 public class mario_script : MonoBehaviour
 {
     Animator m_animator;
+    Rigidbody2D _rigid;
+    BoxCollider2D m_collider;
+    SpriteRenderer spriteRenderer;
+    [SerializeField]
+    float jumpForce = 3.0f;
+    [SerializeField]
     bool isRunning;
     int countFrame = 0;
     public float speed;
+    int jumpLeft = 0;
+    bool flip = false;
     
     // Start is called before the first frame update
     void Start()
     {
+        m_collider = gameObject.GetComponent<BoxCollider2D>();
+        _rigid = gameObject.GetComponent<Rigidbody2D>();
         m_animator = gameObject.GetComponent<Animator>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         isRunning = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-            transform.position += move * speed * Time.deltaTime;
-            transform.eulerAngles = new Vector3(0, 180, 0);
-            isRunning = true;
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-            transform.position += move * speed * Time.deltaTime;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput < 0) flip = true;
+        else if (horizontalInput > 0) flip = false;
+        m_animator.SetFloat("Move", Mathf.Abs(horizontalInput));
+        spriteRenderer.flipX = flip;
+        
+        
+
+        if (Input.GetKeyDown(KeyCode.V) && (isGround() || jumpLeft > 0)) {
+            // if (m_animator.GetBool("isJump")) {
+            //     m_animator.SetTrigger("isDoubleJump");
+            // } else 
+            m_animator.SetBool("isJump", true);
+            _rigid.velocity = new Vector2(_rigid.velocity.x, jumpForce);
+            jumpLeft = jumpLeft - 1;
             
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            isRunning = true;
-        } else if (Input.GetKey(KeyCode.C)) {
-            m_animator.SetTrigger("isShoot");
+        }   
+    
+        
+        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+        
+    }
+    void LateUpdate() {
+        isGround();
+    }
+    bool isGround() {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, m_collider.bounds.extents.y + 0.1f, 1<<8);
+        if (hitInfo.collider != null) {
+            jumpLeft = 0;
+            m_animator.SetBool("isJump", false);
+
+            return true;
         } else {
-            isRunning = false;
+            return false;
         }
-        m_animator.SetBool("isRunning", isRunning);
     }
 }
